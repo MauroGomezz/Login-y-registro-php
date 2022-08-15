619e3c47fb9085ab07bea36c69bd89bb5b7bb611
Kuma <98374456+MauroGomezz@users.noreply.github.com>
2022-08-15 19:17:38 -0300

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conexion = mysqli_connect("localhost","root","","login");
+$conexion = mysqli_connect("blqlhibey3pz190vl2z7-mysql.services.clever-cloud.com","um40idywwnkifqo4","QyRFzfwrB9ozo9vGgWaR","blqlhibey3pz190vl2z7");
 
 ?>
